623ad99011a3be0d1c038cc1dab0c72a64f71607
Magomedcoder <info@magomedcoder.ru>
2022-10-23 18:43:07 +0300

    first commit

---
+++ src/Server.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PCore\JsonRpc;
+
+/**
+ * Class Constant
+ * @package PCore\JsonRpc
+ * @github https://github.com/pcore-framework/json-rpc
+ */
+class Server
+{
+
+}
